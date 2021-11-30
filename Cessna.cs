a0bfbdf51714fe4c7aab86d23eda2031745cd548
Kaitlyn Long <katielong11@gmail.com>
2021-11-30 09:46:22 -0600

    completed inital setup code for Garys Garage

---
+++ Cessna.cs
@@ -0,0 +1,12 @@
+namespace Garage
+{
+    public class Cessna : Vehicle // Propellor light aircraft
+    {
+        public double FuelCapacity { get; set; }
+
+        public void RefuelTank()
+        {
+            // method definition omitted
+        }
+    }
+}
